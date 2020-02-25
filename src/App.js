b5483f33730938897eeb9491826e4ca43ce8ebee
jose2007kj <jose2007kj@gmail.com>
2020-02-25 17:50:29 +0530

    added hooks instead of classes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import {ModelComponent} from './utils'
-class App extends Component {
- constructor(props){
-        super(props);
-        this.state ={
-            show_model: false
-        }
-        this.closeModel = this.closeModel.bind(this);
-        this.openModel = this.openModel.bind(this);
-    }
-    closeModel(){
-        this.setState({
-            show_model: false
-        })
-    }
-    openModel(){
-        this.setState({
-            show_model: true
-        })
-    }
-render(){
+function App() {
+    const [show, setShow] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -30,19 +12,11 @@ render(){
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      <Button onClick = {this.openModel}>Book</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ModelComponent open={this.state.show_model} close={this.closeModel} url="https://meet-with-cybermonkeys.appointlet.com/"/>
+      <Button onClick ={()=>setShow(true) }>Book</Button>
+                <ModelComponent open={show} close={()=>setShow(false)} url="https://meet-with-cybermonkeys.appointlet.com/"/>
       </header>
     </div>
-  );}
+  );
 }
 
 export default App;
